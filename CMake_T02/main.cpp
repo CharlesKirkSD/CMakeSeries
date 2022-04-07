a0c65a51896117472e53da06bcb57e4ddc1ebd16
CharlesKirkSD <charlesackirk@gmail.com>
2022-04-07 14:18:54 +0100

    CMakeSeries T02 : Windows 11 CMake Build

---
+++ CMake_T02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b)
+{
+    return a + b;
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello from C++20 with CMake on Linux/Windows" << std::endl;
+    std::cout << "The sum is : " << add(7,5) << std::endl;
+    std::cout << "Additional line added in WSL Ubuntu to test Git!" << std::endl; 
+    return 0;
+}
