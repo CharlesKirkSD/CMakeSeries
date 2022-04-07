8ceda3b106fc14ec742684156b0d8e0abdf77195
CharlesKirkSD <charlesackirk@google.com>
2022-04-07 18:58:11 +0100

    CMakeSeries T02 : WSL Linux Ubuntu Changes and Build

--- CMake_T02/main.cpp
+++ CMake_T02/main.cpp
@@ -11,7 +11,7 @@ T add(T a, T b)
 int main(int argc, char* argv[])
 {
     std::cout << "Hello from C++20 with CMake on Linux/Windows" << std::endl;
-    std::cout << "The sum is : " << add(7,5) << std::endl;
-    std::cout << "Additional line added in WSL Ubuntu to test Git!" << std::endl; 
+    std::cout << "The sum is : " << add(7,3) << std::endl;
+    std::cout << "add(7,5) changed to add(7,3) in WSL linux Ubuntu to testusing git / GitHub for cross platform development" << std::endl;
     return 0;
 }
